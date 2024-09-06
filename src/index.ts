6f0a83645df1991cb2a0ae5141e5270682677545
Luiz Ricardo Brugnera <luiz.r.brugnera@gmail.com>
2024-09-06 08:21:03 -0300

    feat: integration

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import routes from "./routes/routes";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/v1", routes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
